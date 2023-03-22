4d4f2b13096cec790cfc68a6fcff5f47e1b47aff
Walmyr <wlsf82@gmail.com>
2023-03-22 21:54:44 +0100

    Improve test description

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,4 +1,4 @@
-it('searching for "javascript" returns results', () => {
+it(`searching for "javascript" on ${Cypress.env('ENVIRONMENT')} environment returns ${Cypress.env('MAX_RESULTS')} results`, () => {
   cy.log(`Running against ${Cypress.env('ENVIRONMENT')} environment`)
 
   cy.intercept('GET', '**/search**').as('getStories')
