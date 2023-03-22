1b45b8efc03ab2c112c92309845e310b34e271b5
Walmyr <wlsf82@gmail.com>
2023-03-22 23:03:18 +0100

    Remove unnecessary log
    Now that such info is available in the test's description.

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -2,8 +2,6 @@ const env = Cypress.env('ENVIRONMENT')
 const maxResults = Cypress.env('MAX_RESULTS')
 
 it(`searching for "javascript" on ${env} environment returns ${maxResults} results`, () => {
-  cy.log(`Running against ${env} environment`)
-
   cy.intercept('GET', '**/search**').as('getStories')
   cy.visit('/')
   cy.wait('@getStories')
